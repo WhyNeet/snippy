4e9157832dff5451560ed30d0a28e2e8a80276de
Yet Another Stupid Coder <117515380+WhyNeet@users.noreply.github.com>
2024-06-04 22:49:08 +0300

    chore(desktop): change window title

--- crates/desktop/src/launch.rs
+++ crates/desktop/src/launch.rs
@@ -1,10 +1,12 @@
-use gpui::WindowOptions;
+use gpui::{Action, Menu, MenuItem, WindowOptions};
 
 use crate::{state::State, workspace::Workspace};
 
 pub fn launch(app: gpui::App) {
     app.run(|cx| {
         cx.open_window(WindowOptions::default(), |cx| {
+            cx.set_window_title("snippy");
+
             State::init(cx);
 
             Workspace::build(cx)
